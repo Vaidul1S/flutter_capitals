048c52e9ffd54d623fdd8721240752696f92f6d7
Vaidul1S <vaidas.ad25@gmail.com>
2026-07-24 10:41:13 +0300

    07.24

---
+++ lib/navbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_capitals/notifiers.dart';
+
+class NavBarWidget extends StatelessWidget {
+  const NavBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: selectedPageNotifier,
+      builder: (context, selectedPage, child) {
+        return NavigationBar(
+          height: 60,
+          labelTextStyle: WidgetStatePropertyAll(
+            TextStyle(
+              fontFamily: 'Papyrus',
+              fontWeight: FontWeight.w800,
+              fontSize: 12,
+            ),
+          ),
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.games_outlined, size: 20),
+              label: 'Game',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.copyright_rounded, size: 20),
+              label: 'Credits',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.list_alt_rounded, size: 20),
+              label: 'Sheet',
+            ),
+          ],
+          onDestinationSelected: (int value) {
+            selectedPageNotifier.value = value;
+          },
+          selectedIndex: selectedPage,
+        );
+      },
+    );
+  }
+}
