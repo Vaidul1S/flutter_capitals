0a1845127bd993fd22b3bc9927f54c3ff2d6f01a
Vaidul1S <vaidas.ad25@gmail.com>
2026-08-03 10:59:51 +0300

    08.03

--- lib/pages/sheet.dart
+++ lib/pages/sheet.dart
@@ -1,46 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_capitals/notifiers.dart';
 import '../data/world.dart';
+import '../data/usa.dart';
 
 class SheetPage extends StatelessWidget {
   const SheetPage({super.key});
+
+  List<dynamic> get _currentList =>
+      selectedWorldNotifier.value ? capitals : usCapitals;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: const Color.fromRGBO(68, 107, 119, 0.4),
-      child: Center(
-        child: ListView.builder(
-          padding: const EdgeInsets.all(20),
-          itemCount: capitals.length,
-          itemBuilder: (context, index) {
-            final country = capitals[index];
-            return Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),              
-              child: Column(
-                children: [
-                  Text(
-                    '${country.name} ',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 30,
-                      fontFamily: 'Unkempt Bold',
-                      color: Color.fromRGBO(156, 39, 176, 1),
+      child: Column(
+        children: [
+          IconButton(
+            onPressed: () {
+              selectedWorldNotifier.value = !selectedWorldNotifier.value;
+            },
+            icon: ValueListenableBuilder<bool>(
+              valueListenable: selectedWorldNotifier,
+              builder: (context, selectedWorld, child) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Select List',
+                      style: TextStyle(
+                        fontFamily: 'Unkempt Bold',
+                        color: Color.fromRGBO(156, 39, 176, 1),
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                  Text(
-                    '${country.capital} ',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 30,
-                      fontFamily: 'Unkempt',
-                      color: Color.fromRGBO(182, 133, 28, 1),
+                    const SizedBox(width: 20),
+                    SizedBox(
+                      height: 50,
+                      child: selectedWorld
+                          ? Image.asset('assets/images/world.png')
+                          : Image.asset('assets/images/usa.png'),
                     ),
+                  ],
+                );
+              },
+            ),
+          ),
+          Center(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(20),
+              itemBuilder: (context, index) {
+                final item = _currentList[index];
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: Column(
+                    children: [
+                      Text(
+                        '${item.name} ',
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Unkempt Bold',
+                          color: Color.fromRGBO(156, 39, 176, 1),
+                        ),
+                      ),
+                      Text(
+                        '${item.capital} ',
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Unkempt',
+                          color: Color.fromRGBO(182, 133, 28, 1),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            );
-          },
-        ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
